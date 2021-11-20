1264a23e2a346dd660d3cd3df621bda786dec7b0
KratiGupta <kratigupta8545@gmail.com>
2021-11-20 16:44:30 +0530

    [Krati]Created react app for redux learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const LocalCounter=(props) =>{
+
+  const[ buttonValue,setButtonValue] = React.useState(-10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      the button value is {buttonValue}
+      <button onClick={() =>{
+        setButtonValue(buttonValue+1)
+      }}>Hello World</button>
     </div>
-  );
+  )
+    
+  
+
+
 }
 
-export default App;
+export default LocalCounter;
